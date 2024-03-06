5a4156c10b49d69713b0b9a788105a2e410a81a0
LarsGJobloop <lars.gunnar@jobloop.no>
2024-03-06 11:48:33 +0100

    add backward and forward navigation

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('Hello!');
+function setStage(newStage) {
+    // Hent ut form elementet
+    const form = document.querySelector("#multi-step-form")
+
+    // // Fjerner det nåværende css klassen
+    form.classList.remove("current-form-1")
+    form.classList.remove("current-form-2")
+    form.classList.remove("current-form-3")
+    form.classList.remove("current-form-4")
+
+    // // Legg til den nye css klassen
+    const newClass = "current-form-" + newStage
+    form.classList.add(newClass)
+}
